0a7101bf211e9835a59fe93772043b278fe937e3
Joshua Aguilar <aguilar60080@gmail.com>
2023-03-22 21:00:26 -0700

    Commit

--- q1.hpp
+++ q1.hpp
@@ -5,7 +5,30 @@ using namespace std;
 //********************
 // Declare Function Prototypes
 //********************
+void getinput(int &n1, int &n2)
+{
+    cin >> n1;
+    cin >> n2;
+}
+void getinput(float &f1, float &f2)
+{
+    cin >> f1;
+    cin >> f2;
+}
 
 // ******************************
 // Implement all your functions here
 // ******************************
+
+void swapTwoValues(int &n1, int &n2)
+{
+    int temp = n1;
+    n1 = n2;
+    n2 = temp;
+}
+void swapTwoValues(float &f1, float &f2)
+{
+    float temp = f1;
+    f1 = f2;
+    f2 = temp;
+}
